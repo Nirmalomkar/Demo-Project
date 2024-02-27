797ac8bdb27de413f87817620004312d2f45e857
Omkar Nirmal <96383141+Nirmalomkar@users.noreply.github.com>
2024-02-27 11:40:12 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -2,6 +2,7 @@
    sudo apt update
    sudo apt-get install nginx
    sudo systemctl start nginx
+   sudo rm-rf /etc/nginx/nginx.conf 
    sudo apt install unzip -y
    sudo wget https://www.free-css.com/assets/files/free-css-templates/download/page296/oxer.zip
    sudo unzip oxer.zip
